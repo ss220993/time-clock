651776bc52255de4d311a0385053586628022017
subha <subha220993@gmail.com>
2021-11-15 16:10:42 +0530

    Modify environment config for application

--- config/application.rb
+++ config/application.rb
@@ -1,28 +1,20 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
-module PostitTemplate
+module TimeClockyApp
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
     config.time_zone = 'Eastern Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Tealeaf note: Bootstrap sass gem addition
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.autoload_paths << Rails.root.join('app/services')
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
- 
